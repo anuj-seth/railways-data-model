80b3cfc2d4c91e91d8dd5a6ae6a02ba8de1722f0
anuj-seth <aseth@tavisca.com>
2020-09-20 18:08:52 +0530

    create repository

---
+++ tables.sql
@@ -0,0 +1,13 @@
+create table staging_trains
+(train_no integer,
+ train_name text,
+ seq smallint,
+ station_code text,
+ station_name text,
+ arrival_time time without time zone,
+ departure_time time without time zone,
+ distance smallint,
+ source_station text,
+ source_station_name text,
+ destination_station text,
+ destination_station_name text);
